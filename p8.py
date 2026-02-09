a56a99ce2396fc83e14c64a364cdfbf9830c1416
Shifa2215 <Shifakousar246@gmail.com>
2026-02-09 11:18:11 +0530

    Add files via upload

---
+++ p8.py
@@ -0,0 +1,25 @@
+class Payment:
+    def pay(self):
+        print("Making a payment")
+
+class GooglePay(Payment):
+    def pay(self):
+        print("Payment made using Google Pay")
+
+class PhonePe(Payment):
+    def pay(self):
+        print("Payment made using PhonePe")
+
+class CreditCard(Payment):
+    def pay(self):
+        print("Payment made using Credit Card")
+
+p = Payment()
+g = GooglePay()
+ph = PhonePe()
+c = CreditCard()
+
+p.pay()
+g.pay()
+ph.pay()
+c.pay()
