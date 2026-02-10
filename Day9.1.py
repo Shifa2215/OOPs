adf50a6218a40f3d70f3e33b2af47a0515683ad3
Shifa2215 <Shifakousar246@gmail.com>
2026-02-10 17:50:04 +0530

    Add files via upload

---
+++ Day9.1.py
@@ -0,0 +1,17 @@
+class Book:
+    def __init__(self, title, author, price):
+        self.title = title
+        self.author = author
+        self.price = price
+
+    def __str__(self):
+        return f"Book: {self.title} by {self.author}"
+
+    def __repr__(self):
+        return f"Book(title='{self.title}', author='{self.author}', price={self.price})"
+
+b1 = Book("Python Basics", "John", 500)
+b2 = Book("OOP Concepts", "Alice", 600)
+
+print(b1)
+print([b1, b2])
